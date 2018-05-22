98b66e85444cc60c11e4b90d501761b2d0963711
Thomas Gardiner <thomgardiner@gmail.com>
2018-05-22 12:39:49 -0600

    added methods to deck object

--- deckexample.js
+++ deckexample.js
@@ -1,54 +1,71 @@
-const deckOne  = {
-    items : [
-        {id: 0,
-        currentPile: 0,
-        front: "What is this card? 0",
-        back: "It's an example",
-        points: 2},
-        {id: 1,
-        currentPile: 0,
-        front: "What is this card? 1",
-        back: "It's an example",
-        points: 1},
-        {id: 2,
-        currentPile: 0,
-        front: "What is this card? 2",
-        back: "It's an example",
-        points: 2},
-    ],
-
-    learning : [],
-    know : [],
-    confident : [],
-    memorized : [],
-    mastered : [],
+const Deck = function(){
+    this.items = [];
+    this.learning = [];
+    this.know = [];
+    this.confident = [];
+    this.memorized = [];
+    this.mastered = [];
+    this.addCard = function(front, back){
+        this.items.push({
+            id: this.items.length,
+            currentPile: 0,
+            front: front,
+            back: back,
+            points: 0
+        })
+    },
+    this.findCard = function(id){
+        let card = this.items.filter(function(x){
+            return x.id === id;
+        })
+       card = card[0];
+       if(card !== undefined);
+       return card;
+    }, 
+    this.getRandomCard = function(){
+        let random = Math.floor(Math.random() * this.items.length);
+        return this.findCard(random);
+    },
+    this.drawCard = function(){
+        let card = this.getRandomCard();
+        console.log(card.front);
+    }
 }
 
 
-const findCard = function(deck, id){
-    let card = deck.items.filter(function(x){
-        return x.id === id;
-    })
-   card = card[0];
-   if(card !== undefined)
-   return card;
-}
+// const findCard = function(deck, id){
+//     let card = deck.items.filter(function(x){
+//         return x.id === id;
+//     })
+//    card = card[0];
+//    console.log(card);
+//    return card;
+// }
 
-const getRandomCard = function(deck){
-    let random = Math.floor(Math.random() * deck.items.length);
-    return findCard(deck, random);
-}
+// const getRandomCard = function(deck){
+//     let random = Math.floor(Math.random() * deck.items.length);
+//     return findCard(deck, random);
+// }
 
-const getCardPoints = function(card){
-    console.log(card.points);
-}
+// const getCardPoints = function(card){
+//     console.log(card.points);
+// }
 
-const drawCard = function(deck){
-    let card = getRandomCard(deck);
-    console.log(card.front);
+// const drawCard = function(deck){
+//     let card = getRandomCard(deck);
+//     console.log(card.front);
 
-}
+// }
 
 
-drawCard(deckOne);
+let newDeck = new Deck();
+newDeck.addCard("this is the front", "this is the back");
+newDeck.addCard("this is the front 2", "this is the back 2");
+newDeck.addCard("this is the front 3", "this is the back 3");
+newDeck.addCard("this is the front 4", "this is the back 4");
+
+console.log(newDeck.drawCard());
+
+// deckOne.findCard(1);
+// drawCard(deckOne);
 
