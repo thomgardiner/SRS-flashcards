1f85e730a94ebc7e142684e2b0e329ea9d7bed8f
Thomas Gardiner <thomgardiner@gmail.com>
2018-12-06 20:44:43 -0700

    fixed password issue

--- models/user.js
+++ models/user.js
@@ -3,8 +3,8 @@ const bcrypt = require('bcrypt');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    username: {type: String, required: true, unique: true, max: 15},
-    password: {type: String, required: true, max: 15},
+    username: {type: String, required: true, unique: true, max: 20},
+    password: {type: String, required: true, max: 50},
     email: {type: String, required: true, unique: true, max: 50},
     joined: {type: String, required: true, max: 50},
     decks: []
